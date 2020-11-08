284c044d14524a20ffdf211ef1bbac40c53f244e
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-08 02:30:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
-def square_array(numbers)
+def square_array(numbers) #numbers= [1,2,3,4]
   new_array = []
   counter = 0 
   while counter < numbers.length()
   new_array.push(numbers[counter] * numbers[counter])
   counter += 1
   end 
+  binding.pry
   return new_array
 end
